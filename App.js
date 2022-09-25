f56b98881659574f37e257d1111ca1cb8dc2ef93
emanoel-dev <emanoelps09@gmail.com>
2022-09-25 19:56:09 -0300

    desafio app autoCalculo

--- App.js
+++ App.js
@@ -1,21 +1,150 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { 
+  Text, 
+  View, 
+  Image,
+  Modal, 
+  TextInput,
+  StyleSheet,
+  TouchableOpacity, 
+  KeyboardAvoidingView
+} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Resultado from './src/components/Resultado.js';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        alcool: '',
+        gasolina: '',
+        result: '',
+        statusModal: false,
+      };
+      this.Calcular = this.Calcular.bind(this);
+      this.Fechar = this.Fechar.bind(this);
+}
+Calcular(value) {
+    if (this.state.alcool && this.state.gasolina >= 1 ) {
+
+      this.setState({statusModal : true});
+      const convertAlcool = parseFloat(this.state.alcool);
+      const convertGasolina = parseFloat(this.state.gasolina);
+
+      const calcula = convertAlcool / convertGasolina;
+
+      this.state.result = calcula.toFixed(2);
+
+      this.state.alcool = convertAlcool.toFixed(2);
+      this.state.gasolina = convertGasolina.toFixed(2);
+      
+      // toLocaleString('pt-br', {style: 'currency', currency: 'BRL'});
+
+      console.log(this.state.alcool);
+      
+  } else {
+    alert("Preencha todos os campos!");
+  }
+}
+
+Fechar(visible){
+  this.setState({statusModal : visible});
+
+}
+
+  render() {
+
+    return (
+      <KeyboardAvoidingView 
+      behavior={Platform.OS === "ios" ? "padding" : "height"} // KeyboardAvoidingView faz orietação do teclado aberto em relacao ao sistema operacional iOS/Android
+      style={styles.container}>
+
+        <View style={{width: '100%', height: 280, alignItems: 'center',justifyContent: 'center'}}>
+          <Image style={styles.logo} source={require('./src/images/logo.png')} />
+          <Text style={styles.titulo}>Qual a melhor opção?</Text>
+        </View>
+          <View style={styles.entrada}>
+            <Text style={styles.subTitulo}>Álcool: Preço por Litro</Text>
+            <View style={styles.input}>
+              <TextInput 
+                onChangeText={(value) => this.setState({alcool: value})}
+                keyboardType='numeric' 
+                style={{backgroundColor: '#FFF', width: 350, height: 50, borderRadius: 5, fontSize: 18, padding: 5, marginBottom: 20}}>
+              </TextInput>
+            </View>
+
+            <Text style={styles.subTitulo}>Gasolina: Preço por Litro</Text>
+            <View style={styles.input}> 
+              <TextInput 
+                //placeholder='$ 0.00'
+                onChangeText={(value) => this.setState({gasolina: value})}
+                keyboardType='numeric' 
+                style={{backgroundColor: '#FFF', width: 350, height: 50, borderRadius: 5, fontSize: 18, padding: 5,}}>
+              </TextInput>
+
+              <TouchableOpacity style={styles.botao}>
+                <Text style={{color: '#FFF', fontSize: 28, fontWeight: 'bold'}} onPress={this.Calcular}>Calcular</Text>
+              </TouchableOpacity>
+              <Modal 
+                animationType='slide'
+                visible={this.state.statusModal}>
+
+                <View style={{width: '100%', height: '100%'}}>
+                  <Resultado 
+                    Fechar = { () => this.Fechar(false) }
+                    alcool = {this.state.alcool}
+                    gasolina = {this.state.gasolina}
+                    result = {this.state.result}
+                  />
+                </View>
+              </Modal>
+            </View>
+          </View>
+      </KeyboardAvoidingView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#220901',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    width: 200,
+    height: 200,
+  },
+  titulo: {
+    color: '#FFFFFF',
+    fontSize: 25,
+    fontWeight: 'bold',
+    margin: 20
+  },
+  subTitulo: {
+    color: '#FFFFFF',
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginLeft: 35
+  },
+  input: {
+    width: '100%', 
+    alignItems: 'center', 
+    justifyContent: 'center'
+  },
+  entrada: {
+    width: '100%',
+    height: 250,
+    justifyContent: 'center',
+  },
+  botao: {
+    width: 350,
+    height: 50,
+    backgroundColor: '#BC3908',
+    margin: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5
+  }
 });
---
+++ src/components/Resultado.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
+
+export default class App extends Component {
+    render() {
+        return(
+            <View style={styles.container}>
+
+                <View style={styles.topo}>
+                    <Image 
+                        source={require('../../src/images/gas.png')}
+                        style={styles.logo}
+                    />
+                    <Text style={styles.title}>{this.props.result < 0.7 ? "Compensa usar Alcool" : "Compensa usar Gasolina"}</Text>
+                    <Text style={styles.title}>{this.props.result}</Text>
+                </View>
+                <View style={styles.content}>
+                    <Text style={{color: '#FFF', fontSize: 28, fontWeight: 'bold'}}></Text>
+                    <Text style={styles.produtos}>Álcool: R$ { this.props.alcool }</Text>
+                    <Text style={styles.produtos}>Gasolina: R$ {this.props.gasolina} </Text>
+                </View>
+                <TouchableOpacity style={styles.botao}><Text style={styles.textoBotao} onPress={this.props.Fechar}>Calcular novamente</Text></TouchableOpacity>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#220901',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    topo: {
+        width: '100%',
+        height: 180,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    logo: {
+        width: 200,
+        height: 200,
+    },
+    title: {
+        fontSize: 28,
+        color: '#F6AA1C',
+        fontWeight: 'bold',
+        margin: 15
+    },
+    content: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: 200
+    },
+    produtos: {
+        color: '#FFF',
+        fontSize: 22,
+    },
+    botao: {
+        width: 200,
+        height: 40,
+        borderWidth: 1,
+        borderStyle: 'solid',
+        borderColor: '#BC3908',
+        borderRadius: 5,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    textoBotao: {
+        color: '#BC3908',
+        fontSize: 18,
+    }
+})
